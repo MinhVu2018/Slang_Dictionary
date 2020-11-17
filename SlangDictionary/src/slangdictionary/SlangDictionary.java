8f37d1e4feb027c49eed991905980860647c284e
MinhVu2018 <tvuminht@gmail.com>
2020-11-17 14:08:54 +0700

    UI_01

---
+++ SlangDictionary/src/slangdictionary/SlangDictionary.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slangdictionary;
+
+/**
+ *
+ * @author MinhVu
+ */
+public class SlangDictionary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // call SlangFrame
+        SlangFrame frame = new SlangFrame();
+        
+        // show frame
+        frame.show();
+        
+    }
+    
+}
